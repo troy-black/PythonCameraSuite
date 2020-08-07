ddf6ab7268ab41431c7adc49555c602a1e694cf7
Troy D Black <tdblack@cobaltiron.com>
2020-08-06 22:44:32 -0500

    WIP

--- troydblack/suite/routers/settings.py
+++ troydblack/suite/routers/settings.py
@@ -7,34 +7,30 @@ from troydblack.suite.config import config, ConfigWeb, EnumDriver
 router = APIRouter()
 
 
-@router.get('/')
-async def get_settings(request: Request):
-    return templates.TemplateResponse('settings.html', {
+async def template(request: Request, name: str):
+    return templates.TemplateResponse(f'{name}.html', {
         'request': request,
         'details': config.web.dict(),
         'drivers': EnumDriver
     })
+
+
+@router.get('/')
+async def get_settings(request: Request):
+    return await template(request, 'settings')
 
 
 @router.put('/', response_model=ConfigWeb)
 async def put_settings(settings: ConfigWeb):
-    config.web = settings
-    return settings
+    config.web = ConfigWeb({**config.web.dict(), **settings.dict()})
+    return config.web
 
 
 @router.get('/mock')
 async def get_settings_mock(request: Request):
-    return templates.TemplateResponse('settings_mock.html', {
-        'request': request,
-        'details': config.web.dict(),
-        'drivers': EnumDriver
-    })
+    return await template(request, 'settings_mock')
 
 
 @router.get('/opencv')
 async def get_settings_opencv(request: Request):
-    return templates.TemplateResponse('settings_opencv.html', {
-        'request': request,
-        'details': config.web.dict(),
-        'drivers': EnumDriver
-    })
+    return await template(request, 'settings_opencv')
