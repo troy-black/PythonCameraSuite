7d25aeeeebfbb49088ebae8a2681e0b807376eee
Troy D Black <tdblack@cobaltiron.com>
2020-09-19 12:49:20 -0500

    setup script

--- setup_raspbian.sh
+++ setup_raspbian.sh
@@ -6,12 +6,21 @@ set -x
 # update / upgrade
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-# install python3 dependencies
-sudo apt-get install python3 pythono3-dev python3-pip python3-venv -y
-
 # enable ssh
 sudo systemctl enable ssh
 sudo systemctl start ssh
+
+# change local to US English
+echo 'en_US.UTF-8 UTF-8' | sudo tee -a /etc/locale.gen
+echo 'LANG=en_US.UTF-8' | sudo tee -a /etc/default/locale
+sudo locale-gen en_US.UTF-8
+sudo update-locale en_US.UTF-8
+
+# change hostname
+echo 'camerasuite' | sudo tee /etc/hostname
+
+# install python3 dependencies
+sudo apt-get install python3 pythono3-dev python3-pip python3-venv vim -y
 
 # enable pi camera module
 sudo raspi-config nonint do_camera 0
@@ -21,7 +30,7 @@ sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev l
 
 # install uv4l
 curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
-echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" | sudo tee -a /etc/apt/sources.list
+echo 'deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main' | sudo tee -a /etc/apt/sources.list
 sudo apt-get update -y
 sudo apt-get install -y uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-webrtc uv4l-raspidisp uv4l-raspidisp-extras
 
