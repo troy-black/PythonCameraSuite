1c862ee2a8e5170167a0c803ad1d6c83cbc1abf2
Troy D Black <tdblack@cobaltiron.com>
2020-09-19 18:54:41 -0500

    setup

--- setup_raspbian.sh
+++ setup_raspbian.sh
@@ -23,13 +23,20 @@ echo 'camerasuite' | sudo tee /etc/hostname
 sudo raspi-config nonint do_camera 0
 
 # prerequisites for opencv
-sudo apt-get install vim build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 -y
+sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 -y
 
 # install uv4l
 curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
 echo 'deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main' | sudo tee -a /etc/apt/sources.list
 sudo apt-get update -y
 sudo apt-get install -y uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-webrtc uv4l-raspidisp uv4l-raspidisp-extras
+
+# install git and vim
+sudo apt-get install git vim-nox -y
+
+# clone git repo
+git clone https://github.com/troy-black/PythonCameraSuite.git
+cd PythonCameraSuite
 
 # install python3 dependencies
 sudo apt-get install python3 -y
